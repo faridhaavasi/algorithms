e0e5518d8d947f80c1a23ed20baaa2925a9a1ba0
faridhaavasi <faridhavasi777@gmail.com>
2023-08-03 18:22:26 +0330

    level up

--- limit.py
+++ limit.py
@@ -13,18 +13,32 @@ min = 0
 output:[4,5]
 '''
 
-def limit(arry:list, min=None, max=None):
-    if max is not None:
-        if max in arry:
-            max_index = arry.index(max)
-            return arry[max_index+1:]
-    if min is not None:
-        if min in arry:
-            min_index = arry.index(min)
-            return arry[:min_index+1]
-    if min and min:
-        min_index = arry.index(min)
-        max_index  = arry.index(max)
-        return arry[min_index:min_index]
+# def limit(arry:list, min=None, max=None):
+#     if max is not None:
+#         if max in arry:
+#             max_index = arry.index(max)
+#             return arry[max_index+1:]
+#     if min is not None:
+#         if min in arry:
+#             min_index = arry.index(min)
+#             return arry[:min_index+1]
+#     if min and min:
+#         min_index = arry.index(min)
+#         max_index  = arry.index(max)
+#         return arry[min_index:min_index]
+
+
+def limit(arry, max=None, min=None):
+    l = []
+    if max:
+        for i in arry:
+            if i >=max:
+                l.append(i)
+    if min:
+        for i in arry:
+            if i <=min:
+                l.append(i)
+    return l
+print(limit([1,2,3,4,5],min=3))
 
 
