b8512820907774ecfd41a035f9650f972a3b1866
faridhaavasi <faridhavasi777@gmail.com>
2024-05-09 15:46:35 +0330

    reset projects

--- c.py
+++
@@ -1,9 +0,0 @@
-l = [1,5,6,7,9,12,2,5,6,8]
-m=0
-for i, v in enumerate(l):
-    if i:
-        if v > m:
-            m = v
-
-print(m)
-    
--- sum_taget_index.py
+++
@@ -1,8 +0,0 @@
-nums = [2,5,8,9,1,3,4,7]
-target = 9
-
-for index , num in enumerate(nums):
-    #print(index, num)
-    for j in range(index+1, len(nums)):
-        if nums[index] + nums[j] == target:
-            print([index,j])
