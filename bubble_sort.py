632c2cb64c8e2dc8827d0c44acd0f2eb79aa627e
faridhaavasi <faridhavasi777@gmail.com>
2024-07-24 12:21:33 +0330

    edit

--- bubble_sort.py
+++ bubble_sort.py
@@ -5,9 +5,9 @@ big o(n**2)
 def bubble_sort(l: list) -> list:
     
     for i in range(len(l)):
-        for j in range(i+1, len(l)):
-            if l[i] > l[j]:
-                l[i], l[j] = l[j], l[i]
+
+        for j in range(len(l)-1):
+            if l[j] > l[j+1]:
+                l[i], l[j+1] = l[j+1], l[i]
     return l 
            
-
